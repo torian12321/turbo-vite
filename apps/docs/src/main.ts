ce0fa1edc5e692bb860aaa84bd32606c0d25745f
Aitor <aitorpalomares@hotmail.com>
2024-05-29 17:47:26 +0200

    rename package name

--- apps/docs/src/main.ts
+++ apps/docs/src/main.ts
@@ -1,8 +1,8 @@
-import { Header } from "@repo/ui/header";
+import { Header } from "@propylon/ui/header";
 import "./style.css";
 import typescriptLogo from "/typescript.svg";
-import { Counter } from "@repo/ui/counter";
-import { setupCounter } from "@repo/ui/setup-counter";
+import { Counter } from "@propylon/ui/counter";
+import { setupCounter } from "@propylon/ui/setup-counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
